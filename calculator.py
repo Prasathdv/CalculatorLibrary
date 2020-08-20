377d27ac362a2a370a99e3a4a4686fced68ea19e
Prasath D.V <prasath.dv@gmail.com>
2020-08-20 13:42:30 +0530

    Added unit tests to calculator

--- calculator.py
+++ calculator.py
@@ -4,8 +4,12 @@
     Reference: https://realpython.com/python-continuous-integration/
     This is a simple calculator app which sums and subtract two given numbers.
 '''
+
+
 def addnumbers(num1, num2):
     return num1 + num2
 
+
 def subtnumbers(num1, num2):
     return num1 - num2
+# This is a new line that ends the file.
---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+'''
+    This is unit test module for calculator.py module.
+'''
+
+import calculator
+
+class TestCalculator:
+
+
+    def test_add(self):
+        assert 4 == calculator.addnumbers(2,2)
+
+
+    def test_subt(self):
+        assert 2 == calculator.subtnumbers(4,2)    
