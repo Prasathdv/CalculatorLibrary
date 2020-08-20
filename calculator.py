dfecb29700076c413b3e5f660ea2e84659796172
Prasath D.V <prasath.dv@gmail.com>
2020-08-20 13:13:35 +0530

    Added Calculator module

---
+++ calculator.py
@@ -0,0 +1,11 @@
+'''
+    Date: Aug 20 2020
+    Author: Prasath D.V
+    Reference: https://realpython.com/python-continuous-integration/
+    This is a simple calculator app which sums and subtract two given numbers.
+'''
+def addnumbers(num1, num2):
+    return num1 + num2
+
+def subtnumbers(num1, num2):
+    return num1 - num2
