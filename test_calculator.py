20234a9c0a822cd0126d66109b5791715619fe6a
Prasath D.V <prasath.dv@gmail.com>
2020-08-20 15:18:28 +0530

    Updated test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,14 +2,14 @@
     This is unit test module for calculator.py module.
 '''
 
+
 import calculator
+
 
 class TestCalculator:
 
-
     def test_add(self):
-        assert 4 == calculator.addnumbers(2,2)
-
+        assert 4 == calculator.addnumbers(2, 2)
 
     def test_subt(self):
-        assert 2 == calculator.subtnumbers(4,2)    
+        assert 2 == calculator.subtnumbers(4, 2)
